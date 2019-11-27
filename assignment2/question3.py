bb055800c32214b16c1fb1115be79cc544c38d23
mkk140893github <mkk140893@gmail.com>
2019-11-28 02:43:16 +0500

    removed assigment folders

--- assignment2/question3.py
+++
@@ -1,8 +0,0 @@
-# length of list
-
-
-arr  =  ['a','b','c','d',1,2,3]
-
-length_of_list = len(arr)
-
-print(length_of_lis)t
--- assignment2/question5.py
+++
@@ -1,8 +0,0 @@
-
-#Write a Python program to get the largest number from a numeric list.
-
-array = [10, 20, 4, 45, 99] 
-  
-  
-
-print("Largest Number is:", max(array)) 
